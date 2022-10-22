490a7addbbc9c984eb9178a2802c5737cb5d435f
Seniux <47281748+Seneliux@users.noreply.github.com>
2022-10-22 15:04:21 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -4,3 +4,5 @@ sed -i "s|alias 1| alias ${data}/protected_media/;|g" nginx_proxy
 sed -i "s|alias 2| alias ${data}/;|g" nginx_proxy
 sed -i "s|alias 3| alias ${scanDirectory}/;|g" nginx_proxy
 sed -i "s|alias 4| alias ${data}/nextcloud_media/;|g" nginx_proxy
+
+mv nginx_proxy librephotos
